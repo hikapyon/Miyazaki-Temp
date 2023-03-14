d38c25d2e9f074322382a370d024017013d9432f
hikapyon <hikaru.okafuji@gmail.com>
2023-03-14 22:56:23 +0900

    to be able to see temp of tomorrow and day after tomorrow

--- miyazaki-temp.py
+++ miyazaki-temp.py
@@ -10,3 +10,22 @@ miyazaki_high = soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.temp 
 miyazaki_low = soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.temp > em.low")[0].text
 miyazaki_temp = soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.icon > img")[0].get("alt")
 print("宮崎市の天気は" + miyazaki_temp + "最高気温は" + miyazaki_high + "度、最低気温は、" + miyazaki_low + "度です。")
+
+another_day = input("明日以降の天気を知りたい場合は知りたい日にちを今日から数えた日数で入力してください（例）明日なら1\n")
+if another_day == "1":
+    url = "https://weather.yahoo.co.jp/weather/jp/45/?day=2"
+    an_res = requests.get(url)
+    an_soup = BeautifulSoup(an_res.text,"html.parser")
+    an_miyazaki_high = an_soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.temp > em.high")[0].text
+    an_miyazaki_low = an_soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.temp > em.low")[0].text
+    an_miyazaki_temp = an_soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.icon > img")[0].get("alt")
+    print("宮崎市の" + another_day + "日後の天気は" + an_miyazaki_temp + "最高気温は" + an_miyazaki_high + "度、最低気温は、" + an_miyazaki_low + "度です。")
+
+elif another_day == "2":
+    url = "https://weather.yahoo.co.jp/weather/jp/45/?day=3"
+    an_res = requests.get(url)
+    an_soup = BeautifulSoup(an_res.text,"html.parser")
+    an_miyazaki_high = an_soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.temp > em.high")[0].text
+    an_miyazaki_low = an_soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.temp > em.low")[0].text
+    an_miyazaki_temp = an_soup.select("#map > ul > li.point.pt8710 > a > dl > dd > p.icon > img")[0].get("alt")
+    print("宮崎市の" + another_day + "日後の天気は" + an_miyazaki_temp + "最高気温は" + an_miyazaki_high + "度、最低気温は、" + an_miyazaki_low + "度です。")
